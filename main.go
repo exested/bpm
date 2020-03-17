e8018d0504d2b1e399b7bb3ee952762201b9600a
Alexander Kononov <tempijan@gmail.com>
2020-03-17 10:43:46 +0300

    fix

--- main.go
+++ main.go
@@ -19,7 +19,12 @@ const pattern = "Go"
 const maxGoroutines = 5
 
 
-func patternCounterForUrl(url string, commonChan <-chan struct{}, resChan chan<- urlDataType, wg *sync.WaitGroup) {
+func patternCounterForUrl(
+	url string,
+	goroutines <-chan struct{},
+	resultChan chan<- urlDataType,
+	wg *sync.WaitGroup,
+	) {
 	urlData := urlDataType{
 		url: url,
 		patternCount: 0,
@@ -31,8 +36,8 @@ func patternCounterForUrl(url string, commonChan <-chan struct{}, resChan chan<-
 	res, err := http.Get(url)
 	if err != nil {
 		fmt.Println("URL",  url, "not available")
-		<-commonChan
-		resChan <- urlData
+		<-goroutines
+		resultChan <- urlData
 		return
 	}
 
@@ -40,15 +45,33 @@ func patternCounterForUrl(url string, commonChan <-chan struct{}, resChan chan<-
 	body, err := ioutil.ReadAll(res.Body)
 	urlData.patternCount = strings.Count(string(body), pattern)
 
-	<-commonChan
-	resChan <- urlData
+	resultChan <- urlData
+	<-goroutines
 }
 
+func printResult(resChan <-chan urlDataType, totalCountChan chan<- int)  {
+	totalCount := 0
+	for res := range resChan {
+		totalCount += res.patternCount
+		fmt.Printf("Count for %s: %d\n", res.url, res.patternCount)
+	}
+	totalCountChan <- totalCount
+	close(totalCountChan)
+}
 
 func main() {
 	var wg sync.WaitGroup
-	commonChan := make(chan struct{}, maxGoroutines)
-	resChan := make(chan urlDataType)
+	goroutines := make(chan struct{}, maxGoroutines)
+	resultChan := make(chan urlDataType)
+	totalCountChan := make(chan int, 1)
+
+	defer func() {
+		wg.Wait()
+		close(resultChan)
+		fmt.Printf("Total count: %d\n", <-totalCountChan)
+	}()
+
+	go printResult(resultChan, totalCountChan)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
@@ -57,26 +80,12 @@ func main() {
 			continue
 		}
 		wg.Add(1)
-		commonChan <- struct{}{}
-		go patternCounterForUrl(url, commonChan, resChan, &wg)
+		goroutines <- struct{}{}
+		go patternCounterForUrl(url, goroutines, resultChan, &wg)
 	}
+	close(goroutines)
 
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "error:", err)
-		os.Exit(1)
 	}
-
-	go func() {
-		wg.Wait()
-		close(resChan)
-	}()
-
-	totalCount := 0
-	for res := range resChan {
-		totalCount += res.patternCount
-		fmt.Printf("Count for %s: %d\n", res.url, res.patternCount)
-	}
-
-	fmt.Println("The End")
-	close(commonChan)
 }
