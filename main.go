333de5519d4f8908a8e65b985da03ea49eee7746
Alexander Kononov <tempijan@gmail.com>
2020-03-31 22:48:47 +0300

    review

--- main.go
+++ main.go
@@ -10,82 +10,88 @@ import (
 	"sync"
 )
 
-type urlDataType struct {
+type urlData struct {
 	url string
 	patternCount int
+	error string
 }
 
-const pattern = "Go"
-const maxGoroutines = 5
 
-
-func patternCounterForUrl(
-	url string,
-	goroutines <-chan struct{},
-	resultChan chan<- urlDataType,
-	wg *sync.WaitGroup,
-	) {
-	urlData := urlDataType{
+func getUrl(url string, pattern string) urlData {
+	data := urlData{
 		url: url,
 		patternCount: 0,
+		error: "",
 	}
-	defer func() {
-		wg.Done()
-	}()
 
 	res, err := http.Get(url)
 	if err != nil {
-		fmt.Println("URL",  url, "not available")
-		<-goroutines
-		resultChan <- urlData
-		return
+		data.error = "is not avaliable"
+		return data
 	}
 
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
-	urlData.patternCount = strings.Count(string(body), pattern)
+	data.patternCount = strings.Count(string(body), pattern)
 
-	resultChan <- urlData
-	<-goroutines
+	return data
 }
 
-func printResult(resChan <-chan urlDataType, totalCountChan chan<- int)  {
-	totalCount := 0
-	for res := range resChan {
-		totalCount += res.patternCount
-		fmt.Printf("Count for %s: %d\n", res.url, res.patternCount)
+
+func printResult(results <-chan urlData) {
+	var totalCount int
+	for res := range results {
+		if res.error == "" {
+			fmt.Printf("Count for %s: %d\n", res.url, res.patternCount)
+			totalCount += res.patternCount
+		} else {
+			fmt.Printf("%s: %s\n", res.url, res.error)
+		}
 	}
-	totalCountChan <- totalCount
-	close(totalCountChan)
+	fmt.Printf("Total count: %d\n", totalCount)
 }
 
-func main() {
-	var wg sync.WaitGroup
-	goroutines := make(chan struct{}, maxGoroutines)
-	resultChan := make(chan urlDataType)
-	totalCountChan := make(chan int, 1)
 
-	defer func() {
-		wg.Wait()
-		close(resultChan)
-		fmt.Printf("Total count: %d\n", <-totalCountChan)
-	}()
-
-	go printResult(resultChan, totalCountChan)
-
+func readUrls(urls chan string) {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		url := scanner.Text()
 		if url == "" {
 			continue
 		}
-		wg.Add(1)
-		goroutines <- struct{}{}
-		go patternCounterForUrl(url, goroutines, resultChan, &wg)
+		urls <- url
 	}
-	close(goroutines)
+	close(urls)
+}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "error:", err)
+
+func fetchResult(urls chan string, results chan urlData, maxGoroutines int, pattern string) {
+	goroutines := make(chan int, maxGoroutines)
+	var wg sync.WaitGroup
+
+	for url := range urls {
+		goroutines <- 1
+		wg.Add(1)
+		go func(url string) {
+			results <- getUrl(url, pattern)
+			<-goroutines
+			wg.Done()
+		}(url)
 	}
+	wg.Wait()
+	close(goroutines)
+	close(results)
+}
+
+
+func main() {
+	const pattern = "Go"
+	const maxGoroutines = 5
+	results := make(chan urlData)
+	urls := make(chan string)
+
+	go readUrls(urls)
+	go fetchResult(urls, results, maxGoroutines, pattern)
+
+	printResult(results)
 }
